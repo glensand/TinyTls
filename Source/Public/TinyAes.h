243e5af072311a0ab6ab24b2367eff60f9e24962
glensand <bezborodoff.gleb@gmail.com>
2023-05-08 14:47:59 -0700

    added more comments

--- Source/Public/TinyAes.h
+++ Source/Public/TinyAes.h
@@ -11,14 +11,33 @@ struct evp_cipher_ctx_st;
 
 namespace TinyTls
 {
-
+// A wrapper around openssl library, implements AES ciphering (5 rounds and hardcoded salt, 'cause i want this way)
 class FAes final
 {
 public:
+	/**
+     * @brief Initialize internal structures
+     * @param Key Just an any sequence you want, by this sequence cipher will create a 32byte sequence
+     */
     explicit FAes(const char* Key);
     ~FAes();
 
+	/**
+     * @brief Encrypt the given sequence of data using recently generated key
+     * @param Data Ptr to the buffer to be encrypted
+     * @param Size Size of the buffer [bytes]
+     * @return Pointer to the encrypted sequence, and the Size contains length of the sequence (in bytes)
+     * NOTE! After the work done, you must delete encrypted sequence manually (delete[] seq)
+     */
     unsigned char* Encrypt(const void* Data, int32& Size) const;
+
+	/**
+	* @brief Decrypt the given sequence of data using recently generated key
+	* @param Data Ptr to the buffer to be decrypted
+	* @param Size Size of the buffer [bytes]
+	* @return Pointer to the encrypted sequence, and the Size contains length of the sequence (in bytes)
+	* NOTE! After the work done, you must delete decrypted sequence manually (delete[] seq)
+	*/
     unsigned char* Decrypt(const void* Data, int32& Size) const;
 	
 private:
